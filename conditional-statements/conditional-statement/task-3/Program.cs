cfbcc5b9b8fa4b2967f52e9ecc64a53332792e23
e310905-Saimia <jouni.k.kononen@gmail.com>
2017-09-22 15:02:35 +0300

    Conditional-statement Task 3 done

---
+++ conditional-statements/conditional-statement/task-3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define instructions
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla sekä tarkastaa onko luku parillinen tai pariton.");
+            bool isNumber;
+            Console.Write("Syötä luku: ");
+            // define variables
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+            // Check isNumber
+            if(!isNumber)
+            {
+                Console.WriteLine("Syötit väärän arvon!");
+                Console.ReadKey();
+                return;               
+            }
+
+            // Program logic
+            if (evaluatedNumber < 0)
+            {
+                Console.Write($"Numero {evaluatedNumber} on negatiivinen, ");
+            }
+            else if (evaluatedNumber > 0)
+            {
+                Console.Write($"Numero {evaluatedNumber} on positiivinen, ");
+            }
+            else
+            {
+                Console.Write($"Numero {evaluatedNumber} on nolla, ");
+            }
+
+           
+            if (evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine("se on parillinen");
+            }
+            else
+            {
+                Console.WriteLine("se on pariton");
+            }
+            Console.ReadKey();
+        }
+    }
+}
