50238cc998450be90fc6e3a8572ac0abc3a76c41
e310905-saimia <jouni.k.kononen@gmail.com>
2017-10-06 15:39:41 +0300

    Loop Example added for and while

--- loop-statements/loop-example/loop-example/Program.cs
+++ loop-statements/loop-example/loop-example/Program.cs
@@ -12,14 +12,35 @@ namespace loop_example
             //int.TryParse(userInput, out int number);
             int i = 0;
             int f = 1;
-            do
-            {               
-                i = i + 1;                
-                f = f * i;
-                //Console.WriteLine($"{i}!={f}"); // 5!=120
-            } while (i < number);
 
-            Console.WriteLine($"Syotit: {number}\n Vastaus: {f}"); // 5!=120
+            if (number < 0)
+            {
+                Console.WriteLine("Virheellinen syote");
+            }
+            else
+            {
+                //do
+                //{               
+                //    i = i + 1;                
+                //    f = f * i;
+                //    //Console.WriteLine($"{i}!={f}"); // 5!=120
+                //} while (i < number);
+
+                //while (i<number)
+                //{
+                //    i = i + 1;
+                //    f = f * i;
+                //    Console.WriteLine($"{i}!={f}"); // 5!=120
+                //}
+
+                for(int j=1;j<=number;j++)
+                {
+                    f = f * j;
+                    Console.WriteLine($"{j}!={f}"); // 5!=120
+                }
+                
+                Console.WriteLine($"Syotit: {number}\n Vastaus: {f}"); // 5!=120
+            }
             Console.ReadKey();
         }
     }
