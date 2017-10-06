77dd4ca0546e92ef4ad263a94a391633ee97a0a1
e310905-saimia <jouni.k.kononen@gmail.com>
2017-10-06 14:02:30 +0300

    Conditional statement Task 4 done

---
+++ conditional-statements/conditional-statement/task-4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("If-structure - Task 4.\n");
+
+            Console.Write("Syötä 1. luku: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Syötä 2. luku: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Syötä 3. luku: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            string output;
+
+            if (a < b)
+            {
+                if (b < c)
+                {
+                    output = ($"{a} xxxxx {b} yyyyy {c}");
+                    //Console.WriteLine("{0}, {1}, {2}", a, c, b);
+                    //Console.WriteLine($"{a} xxxxx {b} yyyyy {c}");
+                }
+                else if (a < c)
+                {
+                    output = string.Format("{0}, {1}, {2}", a, c, b);
+                }
+                else
+                {
+                    output = string.Format("{0}, {1}, {2}", c, a, b);
+                }
+            }
+            else
+            {
+                if (a < c)
+                {
+                    output = string.Format("{0}, {1}, {2}", b, a, c);
+                }
+                else if (b < c)
+                {
+                    output = string.Format("{0}, {1}, {2}", b, c, a);
+                }
+                else
+                {
+                    output = string.Format("{0}, {1}, {2}", c, b, a);
+                }
+            }
+
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ loop-statements/loop-example/loop-example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace loop_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ohjelma laskee kertoma. Anna luku, jonka kertoma lasketaan: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number);
+            int i = 0;
+            int f = 1;
+            do
+            {               
+                i = i + 1;                
+                f = f * i;
+                //Console.WriteLine($"{i}!={f}"); // 5!=120
+            } while (i < number);
+
+            Console.WriteLine($"Syotit: {number}\n Vastaus: {f}"); // 5!=120
+            Console.ReadKey();
+        }
+    }
+}
