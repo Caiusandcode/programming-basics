7fc96bd5867b85c466554a1c7ab2aa9ef1d8d78d
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2017-11-20 23:59:29 +0200

    Task 1 and 2 done

---
+++ replacement-tasks/task1/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kysyy lukuja niin kauan, kunnes syötät kaksi samaa lukua peräkkäin.\n");
+            Random rnd = new Random();
+            int oldNumber = rnd.Next();
+            int newNumber = 0;
+            int sumOfNumbers = 0;
+
+            while(true)
+            {
+                Console.Write("Anna luku: ");
+                var userInput = Console.ReadLine();
+                newNumber = int.Parse(userInput);
+                if (newNumber == oldNumber)
+                    break;
+                else
+                {
+                    oldNumber = newNumber;
+                    sumOfNumbers += newNumber;
+                }
+            }
+
+            Console.WriteLine($"Lukujen summa on: {sumOfNumbers}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ replacement-tasks/task1/task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minäpä tiedän luvun väliltä 1-100, jota sinä et tiedä!\n");
+            Random rnd = new Random();
+           
+            string userInput = "";
+
+            do
+            {
+                int rndNumber = rnd.Next(1, 101);
+                int userQuessNumber = 0;
+                int quessCounter = 0;
+                while (true)
+                {
+                    Console.Write("Arvaa luku: ");
+                    userQuessNumber = int.Parse(Console.ReadLine());
+                    if (userQuessNumber < rndNumber)
+                        Console.WriteLine("Lukuni on isompi!");
+                    else if (userQuessNumber > rndNumber)
+                        Console.WriteLine("Lukuni on pienempi!");
+                    else
+                        break;
+                    quessCounter++;
+                }
+
+                Console.WriteLine($"\nOikein! Arvauksia yhteensä: {quessCounter}\n");
+                Console.Write("Pelaatko uuden kierroksen? [Y/N]: ");
+                userInput = Console.ReadLine().ToUpper();
+            } while (userInput == "Y");
+            Console.WriteLine("Ohjelman suoritus päättyy!");
+            Console.ReadKey();
+
+        }
+    }
+}
