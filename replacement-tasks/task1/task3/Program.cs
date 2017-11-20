584cbaff655249b0c6bf811994b859618446f75a
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2017-11-21 00:24:59 +0200

    task 3 - table and function done

---
+++ replacement-tasks/task1/task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tulostetaan taulukosta olevien lukujen verran tähtiä näytölle fuktiossa.");
+            int[] numbers = new int[] { 2, 5, 10, 7, 3 };
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(getNAsterisks(numbers[i]));
+            }
+            Console.ReadKey();
+
+        }
+
+        static string getNAsterisks(int n)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                res += '*';
+            }
+            return res;
+        }
+    }
+}
