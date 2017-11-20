ad27e6bf3c84131becd4538cda04a4d48b10f5e5
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2017-11-21 00:06:25 +0200

    Task 2 added new game option

--- replacement-tasks/task1/task2/Program.cs
+++ replacement-tasks/task1/task2/Program.cs
@@ -8,31 +8,33 @@ namespace task2
         {
             Console.WriteLine("Minäpä tiedän luvun väliltä 1-100, jota sinä et tiedä!\n");
             Random rnd = new Random();
-           
+            int userQuessNumber = 0;
             string userInput = "";
 
             do
             {
-                int rndNumber = rnd.Next(1, 101);
-                int userQuessNumber = 0;
+                int rndNumber = rnd.Next(1, 101);               
                 int quessCounter = 0;
+                
                 while (true)
                 {
                     Console.Write("Arvaa luku: ");
                     userQuessNumber = int.Parse(Console.ReadLine());
+                    quessCounter++;
+
                     if (userQuessNumber < rndNumber)
                         Console.WriteLine("Lukuni on isompi!");
                     else if (userQuessNumber > rndNumber)
                         Console.WriteLine("Lukuni on pienempi!");
                     else
-                        break;
-                    quessCounter++;
+                        break;                    
                 }
 
                 Console.WriteLine($"\nOikein! Arvauksia yhteensä: {quessCounter}\n");
                 Console.Write("Pelaatko uuden kierroksen? [Y/N]: ");
                 userInput = Console.ReadLine().ToUpper();
             } while (userInput == "Y");
+
             Console.WriteLine("Ohjelman suoritus päättyy!");
             Console.ReadKey();
 
